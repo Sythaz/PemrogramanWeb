9d6459e2645fecc7d9684b2ad2e0b11e776b0e9c
Sythaz <syafiqaldiansyah@gmail.com>
2024-09-20 08:13:41 +0700

    Jobsheet-4_Soal - 3.2

--- dasarWeb/Pertemuan4/operator.php
+++ dasarWeb/Pertemuan4/operator.php
@@ -15,4 +15,20 @@ echo "Hasil kali = $hasilkali <br>";
 echo "Hasil bagi = $hasilBagi <br>";
 echo "Sisa bagi = $sisaBagi <br>";
 echo "Hasil pangkat = $pangkat <br>";
+
+echo("<br><br>");
+
+$hasilSama = $a == $b;
+$hasilTidakSama = $a != $b;
+$hasilLebihKecil = $a < $b;
+$hasilLebihBesar = $a > $b;
+$hasilLebihKecilSama = $a <= $b;
+$hasilLebihBesarSama = $a >= $b;
+
+echo "Hasil Sama = $hasilSama <br>";
+echo "Hasil Tidak Sama = $hasilTidakSama <br>";
+echo "Hasil Lebih Kecil = $hasilLebihKecil <br>";
+echo "Hasil Lebih Besar = $hasilLebihBesar <br>";
+echo "Hasil Lebih Kecil Sama = $hasilLebihKecilSama <br>";
+echo "Hasil Lebih Besar Sama = $hasilLebihBesarSama <br>";
 ?>
