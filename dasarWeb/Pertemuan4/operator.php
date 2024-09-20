c9762169a7489e3591f7b0b73a04eef9bff45be7
Sythaz <syafiqaldiansyah@gmail.com>
2024-09-20 08:07:04 +0700

    Jobsheet-4_Soal - 3.1

---
+++ dasarWeb/Pertemuan4/operator.php
@@ -0,0 +1,18 @@
+<?php
+$a = 10;
+$b = 5;
+
+$hasilTambah = $a + $b;
+$hasilKurang = $a - $b;
+$hasilkali = $a * $b;
+$hasilBagi = $a / $b;
+$sisaBagi = $a % $b;
+$pangkat = $a ** $b;
+
+echo "Hasil penjumlahan = $hasilTambah <br>";
+echo "Hasil pengurangan = $hasilKurang <br>";
+echo "Hasil kali = $hasilkali <br>";
+echo "Hasil bagi = $hasilBagi <br>";
+echo "Sisa bagi = $sisaBagi <br>";
+echo "Hasil pangkat = $pangkat <br>";
+?>
