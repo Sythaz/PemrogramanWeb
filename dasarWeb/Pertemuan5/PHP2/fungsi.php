4da240a0deaa9ac260156523b58ad357a7337dbd
Sythaz <syafiqaldiansyah@gmail.com>
2024-09-25 17:40:58 +0700

    Jobsheet 5 (PHP2)_Soal - 3

--- dasarWeb/Pertemuan5/PHP2/fungsi.php
+++ dasarWeb/Pertemuan5/PHP2/fungsi.php
@@ -1,5 +1,5 @@
 <?php
-    function perkenalan($nama, $salam){
+    function perkenalan($nama, $salam="Assalamualaikum"){
         echo $salam . ", ";
         echo "Perkenalkan, nama saya " . $nama . "<br/>";
         echo "Senang berkenalan dengan Anda<br/>";
@@ -7,9 +7,10 @@
 
     perkenalan("Syafiq", "Hallo");
 
-    echo "<hr>";
+    echo "<hr>"; //garis
 
     $saya = "Syafiq";
     $ucapanSalam = "Selamat Pagi";
-    perkenalan($saya, $ucapanSalam);
+
+    perkenalan($saya);
 ?>
