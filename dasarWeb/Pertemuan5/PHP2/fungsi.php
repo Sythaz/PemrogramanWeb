4280f31152b7c6833a5cec8dd50e2cf57e5f95b5
Sythaz <syafiqaldiansyah@gmail.com>
2024-09-25 17:55:02 +0700

    Jobsheet 5 (PHP2)_Soal - 4

--- dasarWeb/Pertemuan5/PHP2/fungsi.php
+++ dasarWeb/Pertemuan5/PHP2/fungsi.php
@@ -1,4 +1,5 @@
 <?php
+/*
     function perkenalan($nama, $salam="Assalamualaikum"){
         echo $salam . ", ";
         echo "Perkenalkan, nama saya " . $nama . "<br/>";
@@ -13,4 +14,14 @@
     $ucapanSalam = "Selamat Pagi";
 
     perkenalan($saya);
+
+    echo "<br><br>";
+*/
+
+    function hitungUmur($thn_lahir, $thn_sekarang){
+        $umur = $thn_sekarang - $thn_lahir;
+        return $umur;
+    }
+
+    echo "Umur saya adalah " . hitungUmur(2004, 2024) . " tahun";
 ?>
