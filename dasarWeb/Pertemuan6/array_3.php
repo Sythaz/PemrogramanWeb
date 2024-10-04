838a4df36a5f16183f720f331da009de0a5c143d
Sythaz <syafiqaldiansyah@gmail.com>
2024-10-04 08:36:39 +0700

    Jobsheet 6_Soal - 3

---
+++ dasarWeb/Pertemuan6/array_3.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <h2>Multidimensional Array</h2>
+        <table>
+            <tr>
+                <th>Judul Film</th>
+                <th>Tahun</th>
+                <th>Rating</th>
+            </tr>
+            <?php
+            $movie = array(
+                array("Avengers: Infinity War", 2018, 8.7),
+                array("The Avengers", 2012, 8.1),
+                array("Guardian of The Galaxy", 2014, 8.3),
+                array("Iron Man", 2008, 7.3)
+            );
+            
+            // echo "<tr>";
+            //     echo "<td>" . $movie[0][0] . "</td>";
+            //     echo "<td>" . $movie[0][1] . "</td>";
+            //     echo "<td>" . $movie[0][2] . "</td>";
+            // echo "</tr>";
+            // echo "<tr>";
+            //     echo "<td>" . $movie[1][0] . "</td>";
+            //     echo "<td>" . $movie[1][1] . "</td>";
+            //     echo "<td>" . $movie[1][2] . "</td>";
+            // echo "</tr>";
+            // echo "<tr>";
+            //     echo "<td>" . $movie[2][0] . "</td>";
+            //     echo "<td>" . $movie[2][1] . "</td>";
+            //     echo "<td>" . $movie[2][2] . "</td>";
+            // echo "</tr>";
+            // echo "<tr>";
+            //     echo "<td>" . $movie[3][0] . "</td>";
+            //     echo "<td>" . $movie[3][1] . "</td>";
+            //     echo "<td>" . $movie[3][2] . "</td>";
+            // echo "</tr>";
+
+            foreach ($movie as $film) {
+                echo "<tr>";
+                echo "<td>{$film[0]}</td>";
+                echo "<td>{$film[1]}</td>";
+                echo "<td>{$film[2]}</td>";
+                echo "</tr>";
+            }
+            ?>
+        </table>
+    </body>
+</html>
