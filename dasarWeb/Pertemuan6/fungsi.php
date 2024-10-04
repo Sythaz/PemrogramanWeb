ee977aa070c796871d1c8da546d82c9f95eaa874
Sythaz <syafiqaldiansyah@gmail.com>
2024-10-04 08:41:11 +0700

    Jobsheet 6_Soal - 4

---
+++ dasarWeb/Pertemuan6/fungsi.php
@@ -0,0 +1,9 @@
+<?php
+function perkenalan(){
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Syafiq<br>";
+    echo "Senang berkenalan dengan anda<br>";
+}
+perkenalan();
+perkenalan();
+?>
