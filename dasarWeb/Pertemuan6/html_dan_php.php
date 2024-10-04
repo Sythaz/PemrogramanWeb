c26ef6ecea6302b78d01fc0ec0d83e9e290542c9
Sythaz <syafiqaldiansyah@gmail.com>
2024-10-04 09:17:51 +0700

    Jobsheet 6_Soal - 17

---
+++ dasarWeb/Pertemuan6/html_dan_php.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cara 01</title>
+    </head>
+    <body>
+        <p>Tanggal hari ini: <?php echo date("d M Y")?></p>
+    </body>
+</html>
+
+<?php
+    echo "<html>";
+    echo "<head><title>Cara 02</title></head>";
+    echo "<body>";
+    echo "<p>Tanggal hari ini: " . date("d M Y") . "</p>";
+    echo "</body>";
+    echo "</html>";
+?>
