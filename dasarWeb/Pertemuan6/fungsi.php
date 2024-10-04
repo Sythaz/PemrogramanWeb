c5fc9de8564112db7a3fe414b8a9212e50647a95
Sythaz <syafiqaldiansyah@gmail.com>
2024-10-04 08:56:57 +0700

    Jobsheet 6_Soal - 7

--- dasarWeb/Pertemuan6/fungsi.php
+++ dasarWeb/Pertemuan6/fungsi.php
@@ -1,4 +1,13 @@
 <?php
+function hitungUmur($thn_lahir, $tahun_sekarang){
+    $umur = $tahun_sekarang - $thn_lahir;
+    return $umur;
+}
+
+echo "Umur saya adalah " . hitungUmur(2004, 2024) . " tahun";
+
+echo "<hr>";
+
 function perkenalan($nama, $salam="Assalamualaikum"){
     echo $salam . ", ";
     echo "Perkenalkan, nama saya " . $nama . "<br>";
