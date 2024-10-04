ef25d2fc0026dcd9c94290fba90aea74538ff0de
Sythaz <syafiqaldiansyah@gmail.com>
2024-10-04 09:02:29 +0700

    Jobsheet 6_Soal - 8

--- dasarWeb/Pertemuan6/fungsi.php
+++ dasarWeb/Pertemuan6/fungsi.php
@@ -4,21 +4,14 @@ function hitungUmur($thn_lahir, $tahun_sekarang){
     return $umur;
 }
 
-echo "Umur saya adalah " . hitungUmur(2004, 2024) . " tahun";
-
-echo "<hr>";
 
 function perkenalan($nama, $salam="Assalamualaikum"){
     echo $salam . ", ";
     echo "Perkenalkan, nama saya " . $nama . "<br>";
+
+    echo "Umur saya adalah " . hitungUmur(2004, 2024) . " tahun<br>";
     echo "Senang berkenalan dengan anda<br>";
 }
-perkenalan("Syafiq", "Hallo");
 
-echo "<hr>";
-
-$saya = "Syafiq";
-$ucapanSalam = "Selamat pagi";
-
-perkenalan($saya);
+perkenalan("Syafiq");
 ?>
