7402c1655766cd1e1f9cfd35cdeb23bb90389635
Sythaz <syafiqaldiansyah@gmail.com>
2024-10-11 13:13:57 +0700

    Jobsheet 7_Soal - 2.2

--- dasarWeb/Pertemuan7/empty.php
+++ dasarWeb/Pertemuan7/empty.php
@@ -5,4 +5,12 @@
     } else {
         echo "Array terdefinisi dan tidak kosong";
     }
+
+    echo "<br>";
+
+    if(empty($nonExistentVar)) {
+        echo "Variabel 'nonExistentVar' tidak ditemukan";
+    } else {
+        echo "Variabel 'nonExistentVar' ditemukan";
+    }
 ?>
