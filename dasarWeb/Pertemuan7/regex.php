1d9a7865c970e0847348a01386939c9817f8cce8
Sythaz <syafiqaldiansyah@gmail.com>
2024-10-11 19:55:51 +0700

    Jobsheet 7_Soal - 5.3

--- dasarWeb/Pertemuan7/regex.php
+++ dasarWeb/Pertemuan7/regex.php
@@ -1,6 +1,11 @@
 <?php
-$pattern = '/[a-z]/';
-$text = 'There are 123 apples.';
+$pattern = '/apple/';
+$replacement = 'banana';
+$text = 'I like apple pie.';
+$new_text = preg_replace($pattern, $replacement, $text);
+echo $new_text; // Output: "I like banana pie."
+
+echo "<br>";
 
 if (preg_match($pattern, $text, $matches)) {
     echo "Cocokkan: " . $matches[0];
