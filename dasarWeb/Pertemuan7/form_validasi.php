282eaf646a43c47fd7988674e8d346a01ed941b7
Sythaz <syafiqaldiansyah@gmail.com>
2024-10-12 14:46:20 +0700

    Jobsheet 7_Soal - 7.2

--- dasarWeb/Pertemuan7/form_validasi.php
+++ dasarWeb/Pertemuan7/form_validasi.php
@@ -1,41 +1,48 @@
-<!DOCTYPE html>
 <html>
     <head>
         <title>Form Input dengan Validasi</title>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" ></script>
     </head>
     <body>
         <h1>Form Input dengan Validasi</h1>
-        <form method="post" action="form_validasi.php">
+        <form id="myForm" method="post" action="form_validasi.php">
         <label for="nama">Nama:</label>
         <input type="text" name="nama" id="nama">
+        <span id="nama-error" style="color : red;"></span>
         <br>
         <label for="email">Email:</label>
         <input type="text" name="email" id="email">
+        <span id="email-error" style="color : red;"></span>
+        
         <br>
         <input type="submit" value="Submit">
         </form>
+
+        <script>
+            $(document).ready(function(){
+                $("#myForm").submit(function(event){
+                    var nama = $("#nama").val()
+                    var email = $("#email").val()
+                    var valid = true
+                    if (nama == "") {
+                        $("#nama-error").text("Nama harus diisi.")
+                        valid = false
+                    } else {
+                        $("#nama-error").text("")
+                    }
+                    
+                    if (email == "") {
+                        $("#email-error").text("Email harus diisi.")
+                        valid = false
+                    } else {
+                        $("#email-error").text("")
+                    }
+                    if (!valid) {
+                        event.preventDefault()
+                    }
+                    
+                })
+            })
+        </script>
     </body>
 </html>
-
-<?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $nama = $_POST['nama'];
-    $email = $_POST['email'];
-    $errors = array();
-    if (empty($nama)) {
-        $errors[] = 'Nama harus diisi.'; 
-    }
-    if (empty($email)) {
-        $errors[] = 'Email harus diisi.';
-    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errors[] = 'Format email tidak valid.';
-    }
-    if (!empty($errors)) {
-        foreach ($errors as $error) {
-            echo $error . '<br>';
-        }
-    } else {
-        echo "Data berhasil dikirim: Nama = $nama, Email = $email";
-    }
-}
-?>
