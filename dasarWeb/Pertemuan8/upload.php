cef0b393d7aac8ad44d03f3f159827e7eae6a914
Sythaz <syafiqaldiansyah@gmail.com>
2024-10-19 10:57:30 +0700

    Jobsheet 8_Soal - 6

--- dasarWeb/Pertemuan8/upload.php
+++ dasarWeb/Pertemuan8/upload.php
@@ -4,7 +4,7 @@ if (isset($_POST['submit'])) {
     $targetfile = $targetdir . basename($_FILES['myfile']['name']); // Menggabungkan path dir dan nama file beserta tipe filenya
     $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION)); // pathinfo mengambil tipe filenya
 
-    $allowedExtensions = array('jpg', 'jpeg', 'png', 'gif');
+    $allowedExtensions = array("txt", "pdf", "doc", "docx");
     $maxsize = 3 * 1024 * 1024; // ini 3 mb
     if (in_array($fileType, $allowedExtensions) && $_FILES['myfile']['size'] <= $maxsize) {
         if (move_uploaded_file($_FILES['myfile']['tmp_name'], $targetfile)) { // Fungsi ini return bool
