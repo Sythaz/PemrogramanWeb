7e3e673b0abe8fba55ed69452ebf26b2f801c9ad
Sythaz <syafiqaldiansyah@gmail.com>
2024-10-19 12:08:35 +0700

    Jobsheet 8_Soal - 11

--- dasarWeb/Pertemuan8/upload_ajax.php
+++ dasarWeb/Pertemuan8/upload_ajax.php
@@ -1,26 +1,31 @@
 <?php
 if (isset(($_FILES['file']))) { // formData dari ajax ditangkap oleh $_FILES
     $errors = array();
-    $file_name = $_FILES['file']['name'];
-    $file_size = $_FILES['file']['size'];
-    $file_tmp = $_FILES['file']['tmp_name'];
-    $file_type = $_FILES['file']['type'];
-    @$file_ext = strtolower("" . end(explode('.', $_FILES['file']['name'])) . "");
-    $extensions = array("pdf", "doc", "docx", "txt");
+    $totalFiles = count($_FILES['file']['name']); // Menghitung total inputan
 
-    if(in_array($file_ext, $extensions) === false) {
-        $errors[] = "Ekstensi file yang diizinkan adalah PDF, DOC, DOCX, atau TXT.";
-    }
-
-    if ($file_size > 2097152) {
-        $errors[] = 'Ukuran file tidak boleh lebih dari 2 MB';
-    }
-
-    if (empty($errors) == true) {
-        move_uploaded_file($file_tmp, "documents/" . $file_name);
-        echo "File berhasil diunggah.";
-    } else {
-        echo implode(" ", $errors);
+    for ($i=0; $i < $totalFiles; $i++) { 
+        $file_name = $_FILES['file']['name'][$i];
+        $file_size = $_FILES['file']['size'][$i];
+        $file_tmp = $_FILES['file']['tmp_name'][$i];
+        $file_type = $_FILES['file']['type'][$i];
+        @$file_ext = strtolower("" . end(explode('.', $_FILES['file']['name'][$i])) . "");
+        
+        $extensions = array("png", "jpeg", "jpg", "gif");
+    
+        if(in_array($file_ext, $extensions) === false) {
+            $errors[] = "Ekstensi file yang diizinkan adalah PDF, DOC, DOCX, atau TXT.";
+        }
+    
+        if ($file_size > 2097152) {
+            $errors[] = 'Ukuran file tidak boleh lebih dari 2 MB';
+        }
+    
+        if (empty($errors) == true) {
+            move_uploaded_file($file_tmp, "documents/" . $file_name);
+            echo "File berhasil diunggah. <br>";
+        } else {
+            echo implode(" ", $errors);
+        }
     }
 }
 ?>
