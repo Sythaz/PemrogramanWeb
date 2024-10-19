cfe99650e87aa82f08c45414278452cb68b8c63a
Sythaz <syafiqaldiansyah@gmail.com>
2024-10-19 10:52:39 +0700

    Jobsheet 8_Soal - 5

--- dasarWeb/Pertemuan8/upload.php
+++ dasarWeb/Pertemuan8/upload.php
@@ -9,7 +9,7 @@ if (isset($_POST['submit'])) {
     if (in_array($fileType, $allowedExtensions) && $_FILES['myfile']['size'] <= $maxsize) {
         if (move_uploaded_file($_FILES['myfile']['tmp_name'], $targetfile)) { // Fungsi ini return bool
             echo 'File berhasil diunggah <br>';
-            
+            echo "<img style='width: 200px;' src='$targetfile'>";
         } else {
             echo 'Gagal mengunggah file';
         }
