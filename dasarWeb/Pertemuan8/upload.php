49033c878f18f9b404c74d01e58724f48048d04f
Sythaz <syafiqaldiansyah@gmail.com>
2024-10-19 10:29:56 +0700

    Jobsheet 8_Soal - 3

--- dasarWeb/Pertemuan8/upload.php
+++ dasarWeb/Pertemuan8/upload.php
@@ -1,12 +1,19 @@
 <?php
 if (isset($_POST['submit'])) {
-    $targetdir = 'uploads/';
-    $targetfile = $targetdir . basename($_FILES['myfile']['name']);
+    $targetdir = 'uploads/'; // Direktori
+    $targetfile = $targetdir . basename($_FILES['myfile']['name']); // Menggabungkan path dir dan nama file beserta tipe filenya
+    $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION)); // pathinfo mengambil tipe filenya
 
-    if (move_uploaded_file($_FILES['myfile']['tmp_name'], $targetfile)) {
-        echo 'File berhasil diunggah';
+    $allowedExtensions = array('jpg', 'jpeg', 'png', 'gif');
+    $maxsize = 3 * 1024 * 1024; // ini 3 mb
+    if (in_array($fileType, $allowedExtensions) && $_FILES['myfile']['size'] <= $maxsize) {
+        if (move_uploaded_file($_FILES['myfile']['tmp_name'], $targetfile)) { // Fungsi ini return bool
+            echo 'File berhasil diunggah <br>';
+        } else {
+            echo 'Gagal mengunggah file';
+        }
     } else {
-        echo 'Gagal mengunggah file';
+        echo 'File tidak valid atau melebihi ukuran maksimum yang diizinkan';
     }
 }
 ?>
